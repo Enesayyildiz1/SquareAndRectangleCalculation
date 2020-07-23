fc8a466eca3e6d2d731ea5a1dc29c7eeb1333ad9
Enesayyildiz1 <66443194+Enesayyildiz1@users.noreply.github.com>
2020-07-23 22:03:20 +0300

    Add project files.

---
+++ SquareAndRectangleCalculation/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace SquareAndRectangleCalculation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxShortEdge = new System.Windows.Forms.TextBox();
+            this.tbxLongEdge = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblField = new System.Windows.Forms.Label();
+            this.lblPerimeter = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tbxRadius = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(259, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cal Square ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(57, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cal Rectangle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(165, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Short Edge:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(165, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Long Edge:";
+            // 
+            // tbxShortEdge
+            // 
+            this.tbxShortEdge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbxShortEdge.Location = new System.Drawing.Point(309, 128);
+            this.tbxShortEdge.Name = "tbxShortEdge";
+            this.tbxShortEdge.Size = new System.Drawing.Size(35, 26);
+            this.tbxShortEdge.TabIndex = 4;
+            // 
+            // tbxLongEdge
+            // 
+            this.tbxLongEdge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbxLongEdge.Location = new System.Drawing.Point(309, 178);
+            this.tbxLongEdge.Name = "tbxLongEdge";
+            this.tbxLongEdge.Size = new System.Drawing.Size(35, 26);
+            this.tbxLongEdge.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(150, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Area:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(150, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Perimeter:";
+            // 
+            // lblField
+            // 
+            this.lblField.AutoSize = true;
+            this.lblField.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblField.Location = new System.Drawing.Point(226, 264);
+            this.lblField.Name = "lblField";
+            this.lblField.Size = new System.Drawing.Size(24, 25);
+            this.lblField.TabIndex = 8;
+            this.lblField.Text = "0";
+            // 
+            // lblPerimeter
+            // 
+            this.lblPerimeter.AutoSize = true;
+            this.lblPerimeter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPerimeter.Location = new System.Drawing.Point(268, 310);
+            this.lblPerimeter.Name = "lblPerimeter";
+            this.lblPerimeter.Size = new System.Drawing.Size(24, 25);
+            this.lblPerimeter.TabIndex = 9;
+            this.lblPerimeter.Text = "0";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(78, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 61);
+            this.button3.TabIndex = 10;
+            this.button3.Text = " Rectangle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(273, 52);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 61);
+            this.button4.TabIndex = 11;
+            this.button4.Text = " Square ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(150, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(397, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Which one would you like to calculate??";
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(456, 52);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(158, 61);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Circle";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // tbxRadius
+            // 
+            this.tbxRadius.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbxRadius.Location = new System.Drawing.Point(309, 222);
+            this.tbxRadius.Name = "tbxRadius";
+            this.tbxRadius.Size = new System.Drawing.Size(35, 26);
+            this.tbxRadius.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(169, 222);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 25);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Radius:";
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(456, 359);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(158, 61);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "Cal Circle";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SpringGreen;
+            this.ClientSize = new System.Drawing.Size(703, 486);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.tbxRadius);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lblPerimeter);
+            this.Controls.Add(this.lblField);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxLongEdge);
+            this.Controls.Add(this.tbxShortEdge);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxShortEdge;
+        private System.Windows.Forms.TextBox tbxLongEdge;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblField;
+        private System.Windows.Forms.Label lblPerimeter;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox tbxRadius;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ SquareAndRectangleCalculation/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SquareAndRectangleCalculation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int shortedge;
+        int longedge;
+        int field, perimeter;
+        const int pi = 3;
+        int r;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            shortedge = Convert.ToInt32(tbxShortEdge.Text);
+             longedge = Convert.ToInt32(tbxLongEdge.Text);
+
+            field = shortedge * longedge ;
+            perimeter =( 2 * shortedge)+ (2 * longedge );
+            lblField.Text = field.ToString();
+            lblPerimeter.Text = perimeter.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            tbxShortEdge.Visible = true;
+            label2.Visible = false;
+            tbxLongEdge.Visible = false;
+            label6.Visible = false;
+            tbxRadius.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            tbxShortEdge.Visible = true;
+            label2.Visible = true;
+            tbxLongEdge.Visible = true;
+            label6.Visible = false;
+            tbxRadius.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            tbxShortEdge.Visible = false;
+
+            label2.Visible = false ;
+            tbxLongEdge.Visible = false ;
+
+            label6.Visible = false;
+            tbxRadius.Visible = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label6.Visible = true;
+            tbxRadius.Visible  = true;
+            label1.Visible = false;
+            tbxShortEdge.Visible = false;
+
+            label2.Visible = false;
+            tbxLongEdge.Visible = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            r = Convert.ToInt32(tbxRadius.Text);
+            field = pi * (r * r);
+            perimeter = 2 * pi * r;
+            lblField.Text = field.ToString();
+            lblPerimeter.Text = perimeter.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            shortedge = Convert.ToInt32(tbxShortEdge.Text);
+            // longedge = Convert.ToInt32(tbxLongEdge.Text);
+
+            field = shortedge * shortedge;
+            perimeter = 4 * shortedge;
+            lblField.Text = field.ToString();
+            lblPerimeter.Text = perimeter.ToString();
+        }
+    }
+}
